4eeb10fc4138dcb44afdd295e8cd16fb6fedf128
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-06-16 16:15:43 -0300

    Update Color.java

--- Color.java
+++ Color.java
@@ -1,10 +1,7 @@
 package onitama;
 import java.io.Serializable;
 
-/**
- *
- * @author vitor
- */
+
 public enum Color implements Serializable, Comparable<Color>{
     RED,
     BLUE,
@@ -14,18 +11,5 @@ public enum Color implements Serializable, Comparable<Color>{
     public static final Color Blue = Color.BLUE;
     public static final Color Red = Color.RED;
     public static final Color None = Color.NONE;
-    
-    static Color ValueOf(String name){
-        if(name==null){
-            throw new NullPointerException("Name não deve ser nulo");
-        }
-         for (Color color : Color.values()) {
-              if (color.name().equalsIgnoreCase(name)) {
-                return color;
-            }
-         }
-        
-          throw new IllegalArgumentException("Nenhum enum com o nome " + Color.class.getName() + "." + name);
-    }
 
 }
