bf0ede5a039c6357b74287fa942be36e94be99dc
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-06-16 20:01:18 -0300

    apaga comentarios

--- Color.java
+++ Color.java
@@ -6,8 +6,6 @@ public enum Color implements Serializable, Comparable<Color>{
     RED,
     BLUE,
     NONE;
-    
-   // Constantes BLUE, RED e NONE
     public static final Color Blue = Color.BLUE;
     public static final Color Red = Color.RED;
     public static final Color None = Color.NONE;
